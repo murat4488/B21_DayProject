bc2ac1d934d416045cb8cf6967242915628088ce
Murat Guney <muratguney@msn.com>
2020-11-12 21:12:47 -0500

    added new feature

--- src/Traning.java
+++ src/Traning.java
@@ -5,8 +5,9 @@ public class Traning {
     public static void main(String[] args) {
 
         String str = "Murat Guney";
+        String str2 = "New feature";
 
-        System.out.println(str);
+        System.out.println(str+" "+"\n"+str2);
 
 
 
