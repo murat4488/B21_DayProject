fcc354d4170cd4b15271a420e89fa3a41f555429
Murat <muratguney@msn.com>
2020-11-12 21:02:59 -0500

    added my name and my surname

---
+++ src/Traning.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Traning {
+
+    public static void main(String[] args) {
+
+        String str = "Murat Guney";
+
+        System.out.println(str);
+
+
+
+
+    }
+}
+
